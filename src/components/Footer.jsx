2a4068e8bbe06381c129990598d44b440de648a6
Minjur <minjur.rahaman99@gmail.com>
2023-03-06 17:52:31 +0600

    footer edited

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,66 +1,104 @@
-import { Box, Button, Container, Divider, Typography } from "@mui/material";
+import { Box, Button, Grid, List, Typography } from "@mui/material";
 import React from "react";
 
 const Footer = () => {
+const dotStyle ={
+            
+    height: "10px",
+    width: "10px",
+    backgroundColor: "rgb(245, 152, 66)",
+    borderRadius: "50%",
+    display: "inline-block",
+    marginRight: "4px"
+  
+};
   return (
-    <Box
-    sx={{
-       bgcolor: 'primary.main',
-       py: 8,
-    }}
- >
-    <Container>
-       <Box
-          sx={{
-             display: 'flex',
-             justiFyContent: 'space-between',
-             alignItems: 'center',
-          }}
-       >
-          <Typography
+    <section style={{backgroundColor: '#f2f5f5', paddingTop: '30px', paddingLeft: '50px'}}>
+      
+      <Grid
+        container
+        spacing={2}
+        style={{
+          paddingTop: "30px",
+          paddingBottom: "30px",
+        }}
+      >
+        <Grid item xs={12} md={6} lg={3} sm={6}>
+        <Typography
              sx={{
                 fontSize: '1.5rem',
-                color: '#fff',
+                color: "black",
                 flex: 1,
+                marginTop: "100px"
              }}
           >
-             Creative Agency
+             Parcel Pro
           </Typography>
-          <Box
-             sx={{
-                display: 'flex',
-                justiFyContent: 'space-between',
-                alignItems: 'center',
-             }}
+        </Grid>
+        <Grid item xs={12} md={6} lg={3} sm={6}>
+          <h3
+            style={{
+              padding: "0px",
+              textAlign: 'left'
+            }}
           >
-             <Typography
-                sx={{
-                   color: 'rgba(255, 255, 255, 0.5)',
-                }}
-             >
-                Creative Agency
-             </Typography>
-             <Button
-                sx={{
-                   bgcolor: 'primary.green',
-                   '&:hover': {
-                      bgcolor: 'primary.green',
-                   },
-                   ml: 2,
-                }}
-             >
-                Get Started
-             </Button>
-          </Box>
-       </Box>
-       <Divider
-          sx={{
-             mt: 3,
-             bgcolor: 'rgba(255,255,255,0.5)'
-          }}
-       />
-    </Container>
- </Box>
+            Company
+          </h3>
+          <div style={{textAlign: "left"}}>
+            <p>
+            <span style={dotStyle}></span>About Us
+            </p>
+            <p>
+                <span style={dotStyle}></span>
+                Contact Us
+            </p>
+            <p><span style={dotStyle}></span>Careers</p>
+            <p><span style={dotStyle}></span> FAQ</p>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} lg={3} sm={6}>
+          <h3
+            style={{
+              padding: "0px",
+              textAlign: "left"
+            }}
+          >
+            Services
+          </h3>
+         <div style={{textAlign: "left"}}>
+            <p><span style={dotStyle}></span> Air Freight</p>
+            <p><span style={dotStyle}></span> Drone Freight</p>
+            <p><span style={dotStyle}></span> Ground Freight</p>
+            <p><span style={dotStyle}></span> Project Logistics</p>
+         </div>
+        </Grid>
+        <Grid item xs={12} md={6} lg={3} sm={6}>
+          <h3
+            style={{
+              padding: "0px",
+              textAlign: "left"
+            }}
+          >
+            Get In Touch
+          </h3>
+          <p
+            style={{
+              padding: "0px",
+              textAlign: "left"
+            }}
+          >
+            +12205 10254 256523 111
+          </p>
+        </Grid>
+      </Grid>
+      <div>
+        <hr />
+       Copyright&copy; All Rights Reserved by Parcel Pro
+        <hr />
+      </div>
+    </section>
+
+    
   );
 };
 
